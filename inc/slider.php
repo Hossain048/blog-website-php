ec9a30b9be9226cf14c2f40ab598b9f0b03740f1
Hossain Mohalder <hossain2271.cse8.bu@gmail.com>
2024-12-04 13:12:41 +0600

    laptop

--- inc/slider.php
+++ inc/slider.php
@@ -5,5 +5,4 @@
             <a href="#"><img src="images/slideshow/03.jpg" alt="nature 3" title="This is slider three Title or Description" /></a>
             <a href="#"><img src="images/slideshow/04.jpg" alt="nature 4" title="This is slider four Title or Description" /></a>
         </div>
-
 </div>
